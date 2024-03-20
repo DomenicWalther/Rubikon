c66c1d7046180b034d18b0189a2cd89e849f604c
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2024-03-20 16:27:58 +0100

    feat: dynamically remove seconds

--- src/lib/components/Timer/Timer.svelte
+++ src/lib/components/Timer/Timer.svelte
@@ -2,6 +2,12 @@
 	let hours = 0;
 	let minutes = 59;
 	let seconds = 30;
+	let totalSeconds;
+	$: totalSeconds = hours * 3600 + minutes * 60 + seconds;
+
+	const removeSecond = () => {
+		seconds -= 1;
+	};
 </script>
 
 <h2>Timer</h2>
@@ -10,4 +16,7 @@
 <p>{hours} Stunden</p>
 <p>{minutes} Minuten</p>
 <p>{seconds} Sekunden</p>
+<p>{totalSeconds} Totale Sekunden</p>
+
+<button on:click={removeSecond}>Remove a second</button>
 
