649a338a40c1ceeb16be1becf4c805d2c870f76a
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2024-03-20 16:25:20 +0100

    feat: make them dynamic

--- src/lib/components/Timer/Timer.svelte
+++ src/lib/components/Timer/Timer.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	let hours = 0;
 	let minutes = 59;
-	let seconds = 0;
+	let seconds = 30;
 </script>
 
 <h2>Timer</h2>
 
 <p>Countdown:</p>
-<p>0 Stunden</p>
-<p>59 Minuten</p>
-<p>0 Sekunden</p>
+<p>{hours} Stunden</p>
+<p>{minutes} Minuten</p>
+<p>{seconds} Sekunden</p>
 
