39d36fb3f2201806f9ed868e72b197eb33c4618c
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2024-03-22 01:37:05 +0100

    feat(backend): Add Models & Postgres DB to Backend

---
+++ backend/database/database.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/domenicwalther/rubikon/backend/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Dbinstance struct {
+	Db *gorm.DB
+}
+
+var DB Dbinstance
+
+func ConnectDB() {
+	dsn := fmt.Sprintf(
+		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database.", err)
+		os.Exit(2)
+	}
+
+	log.Println("Connected to database.")
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("Migrating database...")
+	db.AutoMigrate(&models.Fact{})
+
+	DB = Dbinstance{Db: db}
+}
--- backend/server.go
+++ backend/server.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/domenicwalther/rubikon/backend/database"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	database.ConnectDB()
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
