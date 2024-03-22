0e125babaf3ec81c8bb34d6b37ab0aa3891b7e8f
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2024-03-22 21:17:17 +0100

    dev(backend): finished example API

---
+++ backend/cmd/routes.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/domenicwalther/rubikon/backend/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func setupRoutes(app *fiber.App) {
+
+	app.Get("/", handlers.ListFacts)
+	app.Post("/", handlers.CreateFact)
+}
