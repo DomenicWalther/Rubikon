be81ed3e60e10ba0766b1f4b3beb70f8376a3bb9
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2024-03-23 01:16:06 +0100

    feat(dev): Added User Handler, CreateUser will be ran by a Webhook from Clarke

---
+++ backend/handlers/users.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"github.com/domenicwalther/rubikon/backend/database"
+	"github.com/domenicwalther/rubikon/backend/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func CreateUser(c *fiber.Ctx) error {
+
+	type Response struct {
+		Data struct {
+			ID string `json:"id"`
+		} `json:"data"`
+	}
+
+	var response Response
+	user := new(models.User)
+	if err := c.BodyParser(&response); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	user.User_ID = response.Data.ID
+	database.DB.Db.Create(&user)
+
+	return c.Status(200).JSON(user)
+}
