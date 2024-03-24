50ce36b1f73e1be190cac737054083d1d6fa7e0f
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-03-24 22:39:31 +0100

    fix(backend): allow frontend to fetch backend on localhost

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"github.com/domenicwalther/rubikon/backend/database"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	database.ConnectDB()
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	setupRoutes(app)
 
