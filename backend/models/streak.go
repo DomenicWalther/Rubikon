57d9ca1e461ce9e1459c05989db666629d835a02
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-03-24 22:06:20 +0100

    feat(backend): update Streak/User Model to easier handle requests

--- backend/models/streak.go
+++ backend/models/streak.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Streak struct {
-	gorm.Model
+	ID         uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primary_key;unique"`
 	StreakStart         time.Time
 	StreakEnd           *time.Time
 	StreakLength        int  `gorm:"not null;default:0"`
--- backend/models/user.go
+++ backend/models/user.go
@@ -4,6 +4,9 @@ import "github.com/google/uuid"
 
 type User struct {
 	ID         uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primary_key;unique"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeletedAt  gorm.DeletedAt `gorm:"index"`
 	User_ID    string    `json:"id" gorm:"primary_key;unique"`
 	Level      int       `json:"level" gorm:"not null;default:1"`
 	Experience int       `json:"experience" gorm:"not null;default:0"`
