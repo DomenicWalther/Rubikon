2b214a38b80b0769d637f001ed2f23c7af09326e
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-03-24 22:39:54 +0100

    refactor: user

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,16 +1,21 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	ID         uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primary_key;unique"`
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 	DeletedAt  gorm.DeletedAt `gorm:"index"`
-	User_ID    string    `json:"id" gorm:"primary_key;unique"`
-	Level      int       `json:"level" gorm:"not null;default:1"`
-	Experience int       `json:"experience" gorm:"not null;default:0"`
-	Currency   int       `json:"currency" gorm:"not null;default:0"`
-	Skins      []Skin    `gorm:"many2many:user_skins;"`
-	Streaks    []Streak  `gorm:"foreignKey:UserID;references:ID"`
+	User_ID    string         `json:"id" gorm:"primary_key;unique"`
+	Level      int            `json:"level" gorm:"not null;default:1"`
+	Experience int            `json:"experience" gorm:"not null;default:0"`
+	Currency   int            `json:"currency" gorm:"not null;default:0"`
+	Skins      []Skin         `gorm:"many2many:user_skins;"`
+	Streaks    []Streak       `gorm:"foreignKey:UserID;references:ID"`
 }
