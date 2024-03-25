6660e6b447fcf2476ad35cc962860ee9af11feea
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-03-25 18:48:38 +0100

    fix(backend): Default Time Settings for streak Table

--- backend/models/streak.go
+++ backend/models/streak.go
@@ -8,11 +8,11 @@ import (
 
 type Streak struct {
 	ID                  uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primary_key;unique"`
-	StreakStart         time.Time
+	StreakStart         time.Time `gorm:"autoCreateTime"`
 	StreakEnd           *time.Time
-	StreakLength        int  `gorm:"not null;default:0"`
-	IsUserCurrentStreak bool `gorm:"not null;default:true"`
-	LastStreakDate      time.Time
+	StreakLength        int        `gorm:"not null;default:1"`
+	IsUserCurrentStreak bool       `gorm:"not null;default:true"`
+	LastStreakDate      time.Time  `gorm:"not null;default:now()"`
 	PreviousStreakID    *uuid.UUID `gorm:"type:uuid;"`
 	UserID              string
 }
