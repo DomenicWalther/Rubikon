7741fe10d824155f1bc1dd84fdc64ac364e11697
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-03-29 20:13:11 +0100

    feat(frontend): add progress page

---
+++ frontend/src/routes/app/progress/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Navigation } from '$lib/components';
+</script>
+
+<Navigation />
+<section class="flex justify-center pt-10">
+	<h2 class="text-5xl font-bold">Sam's Fortschritt</h2>
+</section>
