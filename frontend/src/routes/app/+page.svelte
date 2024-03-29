15f3511fd2f91117af16eb29121d3795449cc90d
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-03-29 17:50:13 +0100

    feat(frontend): moved the timer into it's own page where it will ultimately live. showing basic user data on screen now

--- frontend/src/routes/app/+page.svelte
+++ frontend/src/routes/app/+page.svelte
@@ -1,14 +1,23 @@
 <script>
-	import { Timer } from '$lib/components';
 	import UserButton from 'clerk-sveltekit/client/UserButton.svelte';
 	import SignedIn from 'clerk-sveltekit/client/SignedIn.svelte';
-	import SignedOut from 'clerk-sveltekit/client/SignedOut.svelte';
+
+	export let data;
+	let userLevel = data.user.experience;
+	console.log(data.user);
 </script>
 
 <SignedIn>
 	<UserButton afterSignOutUrl="/" />
 </SignedIn>
-<SignedOut>
-	<a href="/sign-in">Sign in</a><span>|</span><a href="/sign-up">Sign up</a>
-</SignedOut>
-<Timer />
+
+<main class="flex items-center flex-col mt-10">
+	<h2>Was machen wir Tobias?</h2>
+	<div class="flex gap-5">
+		<div class="h-40 w-20 bg-mainblue"></div>
+		<p>{userLevel} XP</p>
+	</div>
+	<a href="/app/timer" class="mt-15 bg-mainblue text-white font-bold rounded-lg px-5 py-3 mt-10"
+		>Reise starten</a
+	>
+</main>
