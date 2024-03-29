85ec3730c1c4275c4fd520433040eb39b03d6c93
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-03-29 17:50:44 +0100

    refactor(frontend): change timer page route

---
+++ frontend/src/routes/app/timer/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Timer } from '$lib/components';
+	import SignedIn from 'clerk-sveltekit/client/SignedIn.svelte';
+	import UserButton from 'clerk-sveltekit/client/UserButton.svelte';
+</script>
+
+<SignedIn>
+	<UserButton afterSignOutUrl="/" />
+</SignedIn>
+<Timer />
