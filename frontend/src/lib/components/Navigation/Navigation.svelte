8789f2950170c7f0f86e41d261e2cbb70d5e6f65
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-03-29 20:13:27 +0100

    styling(nav): add pill to show current page

--- frontend/src/lib/components/Navigation/Navigation.svelte
+++ frontend/src/lib/components/Navigation/Navigation.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <nav
-	class="bg-mainblue min-w-[400px] max-w-prose text-white font-bold py-5 rounded-full absolute bottom-10 m-auto left-0 right-0"
+	class="bg-mainblue min-w-[400px] max-w-prose text-white font-bold py-2 rounded-full absolute bottom-10 m-auto left-0 right-0"
 >
 	<ul class="flex justify-around items-center">
 		<UserButton afterSignOutUrl="/" />
-		<li><a href="/">Home</a></li>
-		<li><a href="/">Fortschritt</a></li>
-		<li><a href="/app/leaderboard">Rangliste</a></li>
+		<li class=" py-5 rounded-full px-8"><a href="/app">Home</a></li>
+		<li class="py-5 rounded-full px-8"><a href="/app/progress">Fortschritt</a></li>
+		<li class="py-5 rounded-full px-8 bg-gray-900"><a href="/app/leaderboard">Rangliste</a></li>
 	</ul>
 </nav>
