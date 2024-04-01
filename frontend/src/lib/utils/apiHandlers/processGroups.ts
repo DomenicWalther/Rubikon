dcbfc2097e4c7bb6998f4713b653e49cf0ab0df0
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-04-02 01:09:38 +0200

    refactor(frontend): renamed apiHandler because it handles everything with groups now

--- frontend/src/lib/utils/apiHandlers/processGroups.ts
+++ frontend/src/lib/utils/apiHandlers/processGroups.ts
@@ -18,3 +18,17 @@ export const processGroupCreation = async (groupData: GroupData) => {
     const data = await response.json()
     return data
     }
+
+
+export const processJoinGroup = async(groupId: string) => {
+    const response = await fetch('http://localhost:3000/Groups/join', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${await Clerk.session.getToken()}`
+        },
+        body: JSON.stringify({group_id: groupId})
+    });
+    const data = await response.json()
+    return data
+}
