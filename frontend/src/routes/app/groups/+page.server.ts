901f27d697299ada445e2079fdc2388edbc95530
Domenic Walther <domenicwalthercreatiiv@gmail.com>
2024-04-02 14:26:21 +0200

    refactor(frontend): now fetching user data with user_id

--- frontend/src/routes/app/groups/+page.server.ts
+++ frontend/src/routes/app/groups/+page.server.ts
@@ -1,17 +1,17 @@
-export async function load() {
-    let groups;
-    try {
-        groups = await fetch('http://localhost:3000/Groups');
-        const data = await groups.json();
-        return {
-            body: data
-        }
-    } catch (e) {
-        return {
-            status: 500,
-            body: {
-                message: 'Failed to fetch Group data'
-            }
-        };
-    }
+export async function load(event) {
+	const user_id: string = event.locals.session.userId
+	try {
+		const groups = await fetch(`http://localhost:3000/Groups/${user_id}`);
+		const data = await groups.json();
+		return {
+			body: data
+		}
+	} catch (e) {
+		return {
+			status: 500,
+			body: {
+				message: 'Failed to fetch Group data'
+			}
+		};
+	}
 }
