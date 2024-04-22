a0c69998126418670c9891e8cf1adf4447700d50
IIDaiki <wladik.p97@googlemail.com>
2024-04-22 19:08:28 +0200

    style (frontend) app

--- frontend/src/routes/app/+page.svelte
+++ frontend/src/routes/app/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-	import { Navigation } from '$lib/components';
+	//import { Navigation } from '$lib/components';<Navigation />
+	import Roboter from '$lib/svg/roboter_lobby.svg';
+	import Blitz from '$lib/svg/blitz_lobby.svg';
 
 	export let data;
 	let userExperience = data.user.experience;
@@ -7,16 +9,16 @@
 	let userLevel = Math.floor(Math.sqrt(userExperience / 10) + 1);
 </script>
 
-<Navigation />
-
-<main class="flex items-center flex-col pt-10">
-	<h2>Was machen wir Tobias?</h2>
-	<div class="flex gap-5">
-		<div class="h-40 w-20 bg-mainblue"></div>
-		<p>{userExperience} XP</p>
-		<p>Aktuelles Level:{userLevel}</p>
+<main class="flex items-center flex-col pt-10 h-screen">
+	<div class="mt-10 mb-10"><h1 class="font-bold text-2xl">Was machen wir Tobias?</h1></div>
+	<div class="flex items-center">
+		<img src={Roboter} alt="text" class="w-80 h-80 mb-8">
+		<img src={Blitz} alt="text" class="w-15 h-15">
 	</div>
-	<a href="/app/timer" class="mt-15 bg-mainblue text-white font-bold rounded-lg px-5 py-3 mt-10"
+		<div class="flex items-center h-5 w-80 bg-mainblue rounded-full m-5"></div>
+		<p class="font-medium text-xl">{userExperience} XP</p>
+		<p class="font-medium text-xl">Aktuelles Level:{userLevel}</p>
+	<a href="/app/timer" class="mt-16 bg-mainblue text-white font-bold rounded-full px-5 py-3 mt-10"
 		>Reise starten</a
 	>
 </main>
