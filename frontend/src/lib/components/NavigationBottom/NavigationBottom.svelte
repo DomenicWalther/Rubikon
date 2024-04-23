f2c3ffeebc6b3bf7166ae754ff2408de32b4b12f
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-04-24 01:16:13 +0200

    refactor: remove unused class

--- frontend/src/lib/components/NavigationBottom/NavigationBottom.svelte
+++ frontend/src/lib/components/NavigationBottom/NavigationBottom.svelte
@@ -1,16 +1,16 @@
-
 <script>
-import UserButton from 'clerk-sveltekit/client/UserButton.svelte';
-
+	import UserButton from 'clerk-sveltekit/client/UserButton.svelte';
 </script>
 
-<div class="bottom_navigatiion">
-	<nav class="bg-mainblue min-w-[400px] max-w-prose text-white font-bold py-2 rounded-full relativ bottom-10 m-auto left-0 right-0">
+<div>
+	<nav
+		class="bg-mainblue min-w-[400px] max-w-prose text-white font-bold py-2 rounded-full fixed bottom-10 m-auto left-0 right-0"
+	>
 		<ul class="flex justify-around items-center">
 			<UserButton afterSignOutUrl="/" />
 			<li class=" py-5 rounded-full px-8"><a href="/app">Home</a></li>
 			<li class="py-5 rounded-full px-8"><a href="/app/progress">Fortschritt</a></li>
 			<li class="py-5 rounded-full px-8 bg-gray-900"><a href="/app/leaderboard">Rangliste</a></li>
 		</ul>
-	</nav>	
+	</nav>
 </div>
