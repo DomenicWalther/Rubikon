27a517bef07f3efd64ab1d77bcced89c08eb91b9
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-04-25 01:11:32 +0200

    feat(frontend): add link prop to sidebar items

--- frontend/src/lib/components/NavigationSidebar/SidebarItem.svelte
+++ frontend/src/lib/components/NavigationSidebar/SidebarItem.svelte
@@ -2,11 +2,12 @@
 	export let label;
 	export let svg;
 	export let backgroundColor = 'bg-mainblue';
+	export let link = '/';
 </script>
 
 <li class="flex items-center ml-4">
 	<a href="/"
 		><div class="rounded p-1.5 mr-3 {backgroundColor}"><img src={svg} alt="text" /></div></a
 	>
-	<a href="/">{label}</a>
+	<a href={link}>{label}</a>
 </li>
