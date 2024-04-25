e93036008910668fe977917df246ea11278900c3
IIDaiki <wladik.p97@googlemail.com>
2024-04-25 21:29:52 +0200

    styling: add icons for icontray

---
+++ frontend/src/lib/Svg/Zielscheibe.svelte
@@ -0,0 +1,10 @@
+<svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="10" cy="10" r="10" fill="#F2F6FF"/>
+<circle cx="9.9999" cy="9.9999" r="8.57143" fill="#252C62"/>
+<circle cx="10.0001" cy="10.0001" r="6.42857" fill="#F2F6FF"/>
+<circle cx="10.0001" cy="10.0001" r="4.28571" fill="#252C62"/>
+<circle cx="10" cy="10" r="2.14286" fill="#F2F6FF"/>
+<rect x="10.3629" y="9.72368" width="11.4569" height="0.51323" rx="0.256615" transform="rotate(-44.1837 10.3629 9.72368)" fill="#FDFDFF" stroke="#252C62" stroke-width="0.51323"/>
+<path d="M18.6746 1.63632L18.5928 0.0795666C16.4287 -0.437535 15.5192 2.84267 14.5855 5.49343L18.6746 1.63632Z" fill="#FDFDFF"/>
+<path d="M18.9765 2.00332L20.5301 2.13111C20.9829 4.30963 17.6772 5.1216 15 5.97646L18.9765 2.00332Z" fill="#FDFDFF"/>
+</svg>
