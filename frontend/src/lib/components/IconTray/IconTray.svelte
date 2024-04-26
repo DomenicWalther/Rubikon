a70f9b6a6c1482c6a41a21d6b6431e5c20eaa1ad
IIDaiki <wladik.p97@googlemail.com>
2024-04-26 22:03:12 +0200

    feat (frontend) IconTray Open/Close

--- frontend/src/lib/components/IconTray/IconTray.svelte
+++ frontend/src/lib/components/IconTray/IconTray.svelte
@@ -2,12 +2,55 @@
 	import Shop from '$lib/Svg/Shop.svelte';
 	import Target from '$lib/Svg/Target.svelte';
 	import Hanger from '$lib/Svg/Hanger.svelte';
+	import Arrow from '$lib/Svg/Arrow.svelte';
+	import { onMount } from 'svelte';
+
+	let isIconTrayVisible = false
+
+	function toggleIconTrayVisibility (){
+		isIconTrayVisible = !isIconTrayVisible;
+	}
+
+	onMount (() =>{
+
+	window.addEventListener ('click', handleOutSideClick);
+		return() => {
+
+	window.removeEventListener('click', handleOutSideClick);
+		};
+	});
+
+
+	function
+	handleOutSideClick(event) {
+		const IconTray = document.querySelector ('.Icontray');
+		const button = document.querySelector ('.button');
+
+		if (!
+	IconTray?.contains(event.target)
+	&& !button?.contains (event.target))
+	{
+		isIconTrayVisible = false;
+	}
+	}
+
+
 </script>
 
+
+
+<div class="fixed left-0 flex flex-col justify-center items-center h-full">
+	<button on:click={toggleIconTrayVisibility} class="button">
+		<Arrow/>
+	</button>
+</div>
+
+
+{#if isIconTrayVisible}
 <div
-	class="flex flex-col fixed left-0 bg-mainblue gap-6 rounded-xl items-center pt-4 pl-4 pr-2 pb-4"
->
+	class="fixed left-0 flex flex-col justify-center items-center top-1/2 bg-mainblue gap-6 rounded-xl py-4 pl-2 pr-2 transform -translate-y-1/2">
 	<Shop />
 	<Target />
 	<Hanger />
 </div>
+{/if}
--- frontend/src/routes/app/+page.svelte
+++ frontend/src/routes/app/+page.svelte
@@ -1,8 +1,7 @@
 <script>
-	import { Navigation, NavigationBottom } from '$lib/components';
+	import { Navigation, NavigationBottom, IconTray } from '$lib/components';
 	import Roboter from '$lib/svg/roboter_lobby.svg';
 	import Blitz from '$lib/svg/blitz_lobby.svg';
-	import Pfeil from '$lib/svg/pfeil.svg';
 
 	export let data;
 	let userExperience = data.user.experience;
@@ -12,13 +11,16 @@
 	let nextLevelXP = Math.pow(userLevel, 2) * 10 - Math.pow(userLevel - 1, 2) * 10;
 </script>
 
+
+
+<IconTray/>
+
 <Navigation />
 <main class="flex items-center flex-col h-fit bg-maingrey">
 	<div class="mt-12 mb-10">
 		<h1 class="font-medium text-3xl text-center">Was machen wir <br /> Tobias?</h1>
 	</div>
-	<div class="flex items-center">
-		<img src={Pfeil} alt="text" class="w-14 h-8" />
+	<div class="flex items-center">		
 		<img src={Roboter} alt="text" class="w-40 h-70 mb-8 mr-6 ml-6" />
 		<img src={Blitz} alt="text" class="w-14" />
 	</div>
