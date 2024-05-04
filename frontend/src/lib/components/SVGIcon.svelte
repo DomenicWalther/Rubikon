a83781a1e60fe84837087fb57eb8c4fab2621124
IIDaiki <wladik.p97@googlemail.com>
2024-05-04 18:51:23 +0200

    frontend(navigation): add the ability to identify the current active item in the navbar

--- frontend/src/lib/components/SVGIcon.svelte
+++ frontend/src/lib/components/SVGIcon.svelte
@@ -1,12 +1,14 @@
-<script>
-	export let type;
+<script lang="ts">
+	export let type: string;
+	export let color: string = "white";
+	export let isActiveItem: boolean;
 	let size = 24;
 </script>
 
-{#if type && size}
+{#if type && size && color}
 	{#await import(`$lib/Svg/${type}.svelte`) then module}
 		{#if module.default}
-			<svelte:component this={module.default} width={size} height={size} />
+			<svelte:component this={module.default} width={size} height={size} color={color} {isActiveItem} />
 		{/if}
 	{/await}
 {/if}
