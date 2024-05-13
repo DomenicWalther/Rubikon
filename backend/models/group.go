a469b649d3adca980762138bf93437abc18da0ea
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-05-13 23:27:43 +0200

    refactor(backend): got rid of GroupChat, since every Group only has one Chat i can just store the messages directly

--- backend/models/group.go
+++ backend/models/group.go
@@ -8,35 +8,26 @@ import (
 )
 
 type Group struct {
-	ID          uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid();unique"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   gorm.DeletedAt `gorm:"index"`
-	Name        string         `json:"name" gorm:"not null"`
-	Description string         `json:"description"`
-	OwnerID     string         `json:"owner_id" gorm:"not null"`
-	ImageURL    string         `json:"imageURL"`
-	UserCount   int            `json:"userCount" gorm:"default:1"`
-	Users       []User         `gorm:"many2many:group_users;"`
-	Is_Public   bool           `json:"isPrivate" gorm:"not null;default:true"`
-	GroupChat   GroupChat      `gorm:"foreignkey:GroupID"`
-}
-
-type GroupChat struct {
 	ID                uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid();unique"`
 	CreatedAt         time.Time
 	UpdatedAt         time.Time
-	DeletedAt         gorm.DeletedAt `gorm:"index"`
-	GroupChatMessages []GroupChatMessage
-	GroupID           uuid.UUID
+	DeletedAt         gorm.DeletedAt     `gorm:"index"`
+	Name              string             `json:"name" gorm:"not null"`
+	Description       string             `json:"description"`
+	OwnerID           string             `json:"owner_id" gorm:"not null"`
+	ImageURL          string             `json:"imageURL"`
+	UserCount         int                `json:"userCount" gorm:"default:1"`
+	Users             []User             `gorm:"many2many:group_users;"`
+	Is_Public         bool               `json:"isPrivate" gorm:"not null;default:true"`
+	GroupChatMessages []GroupChatMessage `gorm:"foreignkey:GroupID"`
 }
 
 type GroupChatMessage struct {
-	ID          uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid();unique"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	GroupChatID uuid.UUID
-	DeletedAt   gorm.DeletedAt `gorm:"index"`
-	Message     string         `json:"message" gorm:"not null"`
-	UserID      uuid.UUID      `json:"user_id" gorm:"not null"`
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid();unique"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	GroupID   uuid.UUID      `json:"group_id" gorm:"not null"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Message   string         `json:"message" gorm:"not null"`
+	UserID    string         `json:"user_id" gorm:"not null"`
 }
