124a7b08bb9f148fb358f4246652dbcfa8b312c1
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2024-05-26 15:30:20 +0200

    feat(frontend): use the activeSkin stored in the store

--- frontend/src/routes/app/playground/+page.svelte
+++ frontend/src/routes/app/playground/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import RobotBase from './robot_base.svelte';
 	import SkinImport from '$lib/Svg/Skins/SkinImport.svelte';
-	let activeHat = 'robot_hat_01';
-	let activeMouth = 'robot_mouth_01';
-	let activeEye = 'robot_eyes_01';
+	import { activeSkin } from '$lib/store';
+	$activeSkin = {
+		activeArms: '',
+		activeBGAnimations: '',
+		activeEyes: 'robot_eyes_02',
+		activeHats: 'robot_hat_03',
+		activeMouths: 'robot_mouth_02'
+	};
 	const hats = ['robot_hat_01', 'robot_hat_02', 'robot_hat_03'];
 	const eyes = ['robot_eyes_01', 'robot_eyes_02', 'robot_eyes_03'];
 	const mouths = ['robot_mouth_01', 'robot_mouth_02'];
@@ -12,10 +17,10 @@
 <h1>Skin Playground</h1>
 
 <div class="relative bg-pink-500 h-full w-full mt-14">
-	<SkinImport type={activeHat} skin_type="hats" />
+	<SkinImport type={$activeSkin.activeHats} skin_type="hats" />
 	<RobotBase />
-	<SkinImport type={activeEye} skin_type="eyes" />
-	<SkinImport type={activeMouth} skin_type="mouths" />
+	<SkinImport type={$activeSkin.activeEyes} skin_type="eyes" />
+	<SkinImport type={$activeSkin.activeMouths} skin_type="mouths" />
 </div>
 
 <h3 class="text-4xl font-bold">Available Skins</h3>
@@ -24,7 +29,7 @@
 	<div class="relative flex gap-20">
 		{#each hats as hat}
 			<button
-				on:click={() => (activeHat = hat)}
+				on:click={() => ($activeSkin.activeHats = hat)}
 				class="w-20 h-20 bg-gray-300 justify-center items-center flex rounded-xl p-10"
 			>
 				<SkinImport type={hat} skin_type="hats" isShowcase={true} />
@@ -34,7 +39,7 @@
 	<div class="relative flex gap-20">
 		{#each eyes as eye}
 			<button
-				on:click={() => (activeEye = eye)}
+				on:click={() => ($activeSkin.activeEyes = eye)}
 				class="w-21 h-20 bg-gray-300 justify-center items-center flex rounded-xl p-10"
 			>
 				<SkinImport type={eye} skin_type="eyes" isShowcase={true} />
@@ -44,7 +49,7 @@
 	<div class="relative flex gap-20">
 		{#each mouths as mouth}
 			<button
-				on:click={() => (activeMouth = mouth)}
+				on:click={() => ($activeSkin.activeMouths = mouth)}
 				class="w-23 h-20 bg-gray-300 justify-center items-center flex rounded-xl p-10"
 			>
 				<div class="scale-[300%]">
