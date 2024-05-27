9574e84c6cb2e9a2b0f113e02617626c86ea8b30
IIDaiki <wladik.p97@googlemail.com>
2024-05-27 17:01:16 +0200

    fix (frontend) equals operator for javascript

--- frontend/src/lib/components/NavigationBottom/NavigationBottom.svelte
+++ frontend/src/lib/components/NavigationBottom/NavigationBottom.svelte
@@ -7,11 +7,11 @@
 
 	<nav class="bg-mainblue min-w-[400px] max-w-prose text-white font-bold py-2 fixed m-auto left-0 right-0 bottom-0">
 		<ul class="flex justify-around items-center py-5 relativ">
-			<NavigationBottomItem label="Home" link="/app" svgType="Home" isActive={"home" == activeItem} />
+			<NavigationBottomItem label="Home" link="/app" svgType="Home" isActive={"home" === activeItem} />
 
-			<NavigationBottomItem label="Fortschritt" link="/app/progress" svgType="Progress" isActive={"progress" == activeItem} />
+			<NavigationBottomItem label="Fortschritt" link="/app/progress" svgType="Progress" isActive={"progress" === activeItem} />
 
-			<NavigationBottomItem label="Rangliste" link="/app/leaderboard" svgType="Leaderboard" isActive={"leaderboard" == activeItem} />
+			<NavigationBottomItem label="Rangliste" link="/app/leaderboard" svgType="Leaderboard" isActive={"leaderboard" === activeItem} />
 
 
 		</ul>
