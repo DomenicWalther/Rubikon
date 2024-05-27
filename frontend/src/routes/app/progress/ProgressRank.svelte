545804ebc0f4e1116a9edba46e27732322789552
IIDaiki <wladik.p97@googlemail.com>
2024-05-27 17:02:17 +0200

    styling (frontend) styling progress site

---
+++ frontend/src/routes/app/progress/ProgressRank.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import ProgressRankItems from './ProgressRankItems.svelte';
+	export let currentRank: "Rank_1" | "Rank_2" | "Rank_3" |"Rank_4" | "Rank_5" | "Rank_6" | "Rank_7" |"Rank_8" | "Rank_9" | "Rank_10" | "Rank_11" | "Rank_12" | "Rank_13" ;
+    import {Ranks} from "./ranks"
+    import Lightning from '$lib/Svg/Lightning.svelte';
+</script>
+
+
+{#each Ranks as Rank}
+    <div class="flex justify-center items-center gap-10 mb-14">
+        <div class="w-40 flex justify-end items-center">
+                <div class="bg-mainlightblue w-20 h-3 rounded-md flex items-center relative">
+                    <div class="bg-mainlightblue w-5 h-5 rounded-full absolute right-0"></div>
+                    <div class="bg-mainlightblue w-3 h-40 rounded absolute left-0 top-0"></div>
+                </div>
+        </div>
+    
+        <ProgressRankItems label={Rank.label} svgType={Rank.svgType} isCurrentRank={currentRank === Rank.svgType} />
+    </div>
+{/each}
+
---
+++ frontend/src/routes/app/progress/ProgressRankItems.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let label
+    export let svgType 
+    export let isCurrentRank
+    import SvgIcon from '$lib/components/SVGIcon.svelte';
+</script>
+
+
+<SvgIcon type={svgType} color="white" isActiveItem={false} />
+
+
+<div class="flex flex-col">
+        {#if isCurrentRank}
+        <h3 class="text-2xl font-medium text-mainlightblue">Aktueller</h3>
+        <h3 class="text-2xl font-medium text-mainlightblue">Zustand</h3>
+        {/if}
+        <div class="w-40">
+            <h3 class="text-2xl">{label}</h3>
+        </div>
+</div>
